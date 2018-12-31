a078ee9cdb13e52a9635ed3027410d1b06c3295a
Ali Mahfud <alimahfudpr@gmail.com>
2018-12-31 13:28:03 +0700

    Add component vue to project

---
+++ starbase/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+    <h3>{{ title }}</h3>
+
+    <Character v-for="(character_id, key) in initial_ids" v-bind:id="character_id" v-bind:key="key"/>
+
+    <button class="button is-primary" @click="generateRandom()">Generate Random</button>
+    
+</div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Generate Your Team',
+            initial_ids: [1,11,21]
+        }
+    },
+    components : { Character },
+    methods : {
+        generateRandom() {
+            this.initial_ids.push(this.initial_ids.length + 1)
+        }
+    }
+}
+</script>
---
+++ starbase/src/components/Character.vue
@@ -0,0 +1,38 @@
+<template>
+    <article class="message is-success">
+        <div class="message-header">
+            <p>{{ character.name }}</p>
+            <button class="delete" aria-label="delete"></button>
+        </div>
+        <div class="message-body">
+            <p>Height: {{ character.height }}</p>
+            <p>Mass: {{ character.mass }}</p>
+            <p>Birth Year: {{ character.birth_year }}</p>
+        </div>
+    </article>
+
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            character : {}
+        }
+    },
+    props : ['id'],
+    methods : {
+        fetchCharacter(id) {
+            fetch(`http://swapi.co/api/people/${id}`, {
+                method: 'GET'
+            }).then(response => response.json())
+            .then(json => this.character = json)
+        }
+    },
+    created() {
+        this.fetchCharacter(this.id)
+    }
+}
+</script>
+
